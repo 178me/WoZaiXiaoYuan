903c360c303c1cec5178a4cea0cb805c9c396ea8
178me <1403341393@qq.com>
2021-03-15 19:25:48 +0800

    完善一日三检打卡

--- wozaixiaoyuan.py
+++ wozaixiaoyuan.py
@@ -12,17 +12,19 @@ lib = MyLib()
 #  lib.debug = True
 
 # 发送提醒
+
 def send_warn(text):
     ''' 发送提醒
     :param text: 提醒文本
     :return: void
     '''
-    lib.send_message(text,"tzTCCaL")
+    lib.send_message(text, "tzTCCaL")
     # 发送间隔60秒 因为云函数不允许单次休眠太久 所以分次
     #  time.sleep(16)
     #  time.sleep(16)
     #  time.sleep(16)
     #  time.sleep(16)
+
 
 class WoZaiXiaoYuan():
     ''' 我在校园
@@ -39,10 +41,10 @@ class WoZaiXiaoYuan():
         "Connection": "keep-alive",
         "Referer": "https://servicewechat.com/wxce6d08f781975d91/150/page-frame.html",
         "token": "",  # 此处填写token
-        }
+    }
 
     # 一日三检数据 自行修改
-    sign_data = {
+    heat_data = {
         "answers": '["0"]',
         "seq": "1",
         "temperature": "36.2",
@@ -54,33 +56,33 @@ class WoZaiXiaoYuan():
         "province": "广东省",
         "township": "江海街道",
         "street": "上冲中约新街一巷",
-            }
+    }
 
     # 健康打卡数据 自行修改
     health_data = {
         "answers": '["0"]',
-         "latitude": "23.404326",
-         "longitude": "113.220125",
-         "country": "中国",
-         "city": "广州市",
-         "district": "花都区",
-         "province": "广东省",
-         "township": "花城街道",
-         "street": "公益路",
-         "areacode":"440114",
-         }
+        "latitude": "23.404326",
+        "longitude": "113.220125",
+        "country": "中国",
+        "city": "广州市",
+        "district": "花都区",
+        "province": "广东省",
+        "township": "花城街道",
+        "street": "公益路",
+        "areacode": "440114",
+    }
 
     # 用户信息列表 可设置多用户
     user_info = [
         {
-            "name":"",
-            "token":"",
+            "name": "",
+            "token": "",
             # 抓到口令的日期 此字段影响提醒功能 建议填写
-            "token_date":datetime.datetime.strptime(\
+            "token_date": datetime.datetime.strptime(\
                     "2021-2-10 18:00:00", '%Y-%m-%d %H:%M:%S'),
-            "txtpad_name":"", # 文本派名称 必填
-            "txtpad_pwd":"",
-            },
+            "txtpad_name": "",  # 文本派名称 必填
+            "txtpad_pwd": "",
+        },
     ]
 
     # 获取随机体温
@@ -95,7 +97,7 @@ class WoZaiXiaoYuan():
         ''' 获取打卡时段
         :return: seq值 || 0(不在打卡时间内)
         '''
-        current_hour = datetime.datetime.now() #+ datetime.timedelta(hours=8)
+        current_hour = datetime.datetime.now()  # + datetime.timedelta(hours=8)
         current_hour = current_hour.hour
         if 0 <= current_hour <= 8:
             return "1"
@@ -144,7 +146,7 @@ class WoZaiXiaoYuan():
             # 第一个文本 令牌
             self.user_info[i]["token"] = arr["note_content"].strip()
             # 第二个文本 令牌获取时间
-            self.user_info[i]["token_date"] = datetime.datetime.strptime(\
+            self.user_info[i]["token_date"] = datetime.datetime.strptime(
                 arr["updated_time"], '%Y-%m-%d %H:%M:%S')
 
         def set_txtpad_text(i):
@@ -153,12 +155,14 @@ class WoZaiXiaoYuan():
             :return: void
             '''
             # 修改获取文本的用户名 我的文本标题是token 和 token_date 修改成你自己的
-            token = lib.get_txtpad_text(self.user_info[i]["txtpad_name"],'token')
-            date = lib.get_txtpad_text(self.user_info[i]["txtpad_name"],'token_date')
+            token = lib.get_txtpad_text(
+                self.user_info[i]["txtpad_name"], 'token')
+            date = lib.get_txtpad_text(
+                self.user_info[i]["txtpad_name"], 'token_date')
             # 第一个文本 令牌
             self.user_info[i]["token"] = token[0].strip()
             # 第二个文本 令牌获取时间
-            self.user_info[i]["token_date"] = datetime.datetime.strptime(\
+            self.user_info[i]["token_date"] = datetime.datetime.strptime(
                 date, '%Y-%m-%d %H:%M:%S')
 
         for i in range(len(self.user_info)):
@@ -167,7 +171,7 @@ class WoZaiXiaoYuan():
             # 使用文本派
             #  set_txtpad_text(arr)
 
-    def punch_card(self,punch_card_api,punch_card_info,token,index):
+    def punch_card(self, punch_card_api, punch_card_info, token, index):
         ''' 打卡
         :param url: 打卡的api
         :param data: 打卡提交的数据
@@ -176,11 +180,12 @@ class WoZaiXiaoYuan():
         :return: void
         '''
         # 检查口令状态 手动的自己填写日期
-        token_statue = self.get_token_dead_time(self.user_info[index]["token_date"])
+        token_statue = self.get_token_dead_time(
+            self.user_info[index]["token_date"])
         if token_statue == -1:
             # 发送过期警告
             token_statue = str(
-            self.user_info[index]["name"]+"的口令:" + self.headers["token"]+'\n'+"错误信息:口令过期")
+                self.user_info[index]["name"]+"的口令:" + self.headers["token"]+'\n'+"错误信息:口令过期")
             send_warn(token_statue)
             lib.log("过期")
         elif(token_statue == 0):
@@ -189,7 +194,8 @@ class WoZaiXiaoYuan():
         else:
             # 发送将过期警告
             token_statue += '\n' + \
-                str(self.user_info[index]["name"]+"的口令:" + self.headers["token"])
+                str(self.user_info[index]["name"] +
+                    "的口令:" + self.headers["token"])
             send_warn(token_statue)
             lib.log("即将过期")
         # 设置口令，开始打卡
@@ -213,7 +219,8 @@ class WoZaiXiaoYuan():
         else:
             token_statue = "请求错误:" + response["message"]
             token_statue += '\n' + \
-                str(self.user_info[index]["name"]+"的口令:" + self.headers["token"])
+                str(self.user_info[index]["name"] +
+                    "的口令:" + self.headers["token"])
             send_warn(token_statue)
             print(response)
 
@@ -227,9 +234,22 @@ def main():
     wzxy = WoZaiXiaoYuan()
     # 使用网络剪贴板获取token 不需要的可以手动在user_info添加token并注释此功能
     wzxy.get_token_info()
+    '''
+    如果是调用一日三检打卡
+    wzxy.heat_data["seq"] = wzxy.get_seq()
+    if wzxy.heat_data["seq"] == 0:
+        return 0
+    '''
     # 循环为每个用户打卡
     for index in range(len(wzxy.user_info)):
-        # 调用健康打卡
+        # 调用健康打卡 只需要调用这段代码
         wzxy.punch_card(wzxy.health_url,wzxy.health_data,wzxy.user_info[index]["token"],index)
+        '''
+        如果是调用一日三检打卡
+        wzxy.heat_data["temperature"] = wzxy.get_random_temprature()
+        wzxy.punch_card(wzxy.heat_url, wzxy.heat_data,
+                        wzxy.user_info[index]["token"], index)
+                        '''
+
 
 main()
